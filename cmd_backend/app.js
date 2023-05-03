2039cf8e78aef8dc6eb39f9efc1304a0d8db6893
vijaymanikantareddy <svmreddy7799@gmail.com>
2023-05-03 21:27:46 +0530

    backend_changes

--- cmd_backend/app.js
+++ cmd_backend/app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/logindata');
+
 
 var app = express();
-var cors = require('cors');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/addlogin', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- cmd_backend/routes/index.js
+++ cmd_backend/routes/index.js
@@ -1,25 +1,93 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const router = require('./routers/router')
+var express = require('express');
+var router = express.Router();
 var monk = require('monk')
-const cors = require('cors')
-var db = monk('localhost:27017/car');
-var users = db.get('users')
+var cors = require('cors');
+var db = monk('127.0.0.1:27017/carrentalapp');
+var logindata = db.get('logindata');
+if(db){
+  console.log("Connected")
+}
+// var signup_data = db.get('login_data');
+// var logindata = db.get('Login');
 
 
-router.get('/', function(req, res, next){
-  res.render('index', {title:'Express'})
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-router.post('/adduser', function(req, res){
-    console.log(res.body);
-    users.insert({"name":req.body.users.username}, {"password": req.body.users.password}, function(err, docs){
-        if(err){
-            res.sendStatus(500)
-        }else{
-            res.send(docs)
-        }
-    })
-})
-module.exports = router;
 
+// router.get('/adduser', function(req, res) {
+//   console.log(req.body);
+//   // Code to retrieve data from database
+//   login_data.find({}, function(err, docs1) {
+//     if(err) {
+//       res.sendStatus(500);
+//     } else {
+//       res.send(docs1);
+//     }
+//   });
+// });
+
+
+// router.get('/Login',function(req, res){
+//   console.log(req.body);
+//   logindata.find({},function(err, docs){
+//     if(err){
+//       res.sendStatus(500)
+//     }
+//     else{
+//       res.send(docs)
+//     }
+//   })
+// })
+
+
+router.post('/addlogin',function(req, res){
+  console.log(req.body);
+  logindata.insert({"username":req.body.logindata.username,"password":req.body.logindata.password},function(err, docs){
+    if(err){
+      res.sendStatus(500)
+    }
+    else{
+      res.send(docs)
+    }
+  })
+})
+// router.post('/signup',function(req, res){
+//   console.log(req.body);
+//   signup_data.insert({"username":req.body.signup_data.username,"email":req.body.signup_data.email,"password":req.body.signup_data.password,"confirmPassword":req.body.signup_data.ConfirmPassword},function(err, docs){
+//     if(err){
+//       res.sendStatus(500)
+//     }
+//     else{
+//       res.send(docs)
+//     }
+//   })
+// })
+// router.post('/Signup',function(req, res){
+//   console.log(req.body);
+//   complaints.insert({
+//     // "name":req.body.users.username,"pwd":req.body.users.passowrd
+//     "college":req.body.complaints.college,
+//     "building":req.body.complaints.building,
+//     "location":req.body.complaints.location,
+//     "date":req.body.complaints.date,
+//     "work":req.body.complaints.work,
+//     "priority":req.body.complaints.priority,
+//     "description":req.body.complaints.description,
+//     "remarks":req.body.complaints.remarks
+
+
+
+//       },function(err, docs){
+//     if(err){
+//       res.sendStatus(500)
+//     }
+//     else{
+//       res.send(docs)
+//     }
+//   })
+// })
+
+module.exports = router;
